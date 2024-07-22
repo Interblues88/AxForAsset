193179e3a59c5d26c26b62204d4245f6187c2528
Interblues88 <delvin.setiamin.08@gmail.com>
2024-07-22 14:02:13 +0700

    push home

--- app/src/main/java/com/example/axforasset/MainActivity.java
+++ app/src/main/java/com/example/axforasset/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.axforasset;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Tes Change 1
-//        change 2
-//        hello
-        // ini jenni hehe
+        registerBtn =findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
