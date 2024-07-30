25c8506e493599fe795487099d9b0594070ef649
Interblues88 <delvin.setiamin.08@gmail.com>
2024-07-30 09:13:46 +0700

    push home

--- app/src/main/java/com/example/axforasset/HomeActivity.java
+++ app/src/main/java/com/example/axforasset/HomeActivity.java
@@ -1,12 +1,22 @@
 package com.example.axforasset;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,6 +24,9 @@ public class HomeActivity extends AppCompatActivity {
     ViewPager viewPager;
 
     TabPagerAdapter tabPagerAdapter;
+    ViewPager2 viewPager2;
+
+    private Handler slideHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +38,61 @@ public class HomeActivity extends AppCompatActivity {
 
         viewPager.setAdapter(tabPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        viewPager2 = findViewById(R.id.viewPager2);
+
+        List<SlideItem> slideritem = new ArrayList<>();
+        slideritem.add(new SlideItem(R.drawable.carousel1));
+        slideritem.add(new SlideItem(R.drawable.carousel2));
+        slideritem.add(new SlideItem(R.drawable.carousel3));
+
+        viewPager2.setAdapter(new SlideAdapter(slideritem, viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransform = new CompositePageTransformer();
+        compositePageTransform.addTransformer(new MarginPageTransformer(30));
+        compositePageTransform.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r  = 1-Math.abs(position);
+                page.setScaleY(0.85f + r*0.15f);
+
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransform);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                slideHandler.removeCallbacks(sliderRunnable);
+                slideHandler.postDelayed(sliderRunnable, 3000);
+            }
+        });
+    }
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        slideHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        slideHandler.postDelayed(sliderRunnable, 3000);
     }
 }
--- app/src/main/java/com/example/axforasset/MainActivity.java
+++ app/src/main/java/com/example/axforasset/MainActivity.java
@@ -13,14 +13,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 //        Tes Change 1
 //        change 2
 //        hello
         // ini jenni hehe
 //        setContentView(R.layout.activity_main);
 //        registerBtn =findViewById(R.id.registerBtn);
-
+//
 //        registerBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
