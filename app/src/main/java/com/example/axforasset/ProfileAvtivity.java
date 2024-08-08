dc637841d47e8ca669027774cdd5ad7948c7722b
jnnfr14 <jenni141004@gmail.com>
2024-08-08 08:57:57 +0700

    profile page

---
+++ app/src/main/java/com/example/axforasset/ProfileAvtivity.java
@@ -0,0 +1,14 @@
+package com.example.axforasset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_avtivity);
+    }
+}
